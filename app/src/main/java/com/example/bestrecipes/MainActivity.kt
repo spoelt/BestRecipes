3ebc47318be9e77ec3490aab88570ff22c4bf18b
spoelt <s.poeltinger@gmail.com>
2020-12-17 18:24:46 +0100

    Add image restrictions

--- app/src/main/java/com/example/bestrecipes/MainActivity.kt
+++ app/src/main/java/com/example/bestrecipes/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
@@ -12,6 +13,7 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.example.bestrecipes.R.drawable.happy_meal_small
 import androidx.compose.material.Text
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContent {
             Column {
                 Image(asset = imageResource(
-                    id = happy_meal_small)
+                    id = happy_meal_small),
+                    // add the following lines to restrict the screen real estate that
+                    // the image is allowed to occupy
+                    // (especially important when using larger images!)
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
                 )
                 Column(
                     modifier = Modifier.padding(16.dp)
