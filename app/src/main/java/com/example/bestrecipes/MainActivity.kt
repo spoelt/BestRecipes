c7f861adda2bb3036e0e0194e126f71569e7afb7
spoelt <s.poeltinger@gmail.com>
2020-12-19 11:18:07 +0100

    Add image restrictions

--- app/src/main/java/com/example/bestrecipes/MainActivity.kt
+++ app/src/main/java/com/example/bestrecipes/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.bestrecipes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.example.bestrecipes.R.drawable.happy_meal_small
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +25,17 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
 
         setContent {
-            Column {
-                Image(asset = imageResource(
-                    id = happy_meal_small),
+            ScrollableColumn(
+                modifier = Modifier
+                    //.fillMaxSize()
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(color = Color(0xFFF2F2F2))
+            ) {
+                Image(
+                    asset = imageResource(
+                        id = happy_meal_small
+                    ),
                     // add the following lines to restrict the screen real estate that
                     // the image is allowed to occupy
                     // (especially important when using larger images!)
@@ -33,11 +45,27 @@ class MainActivity : AppCompatActivity() {
                 Column(
                     modifier = Modifier.padding(16.dp)
                 ) {
-                    Text(text = "Happy Meal")
+                    Text(
+                        text = "Happy Meal",
+                        style = TextStyle(
+                            fontSize = TextUnit.Sp(26)
+                        )
+                    )
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "800 Calories")
+                    Text(
+                        text = "800 Calories",
+                        style = TextStyle(
+                            fontSize = TextUnit.Sp(17)
+                        )
+                    )
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "$5.99")
+                    Text(
+                        text = "$5.99",
+                        style = TextStyle(
+                            color = Color.Red,
+                            fontSize = TextUnit.Sp(17)
+                        )
+                    )
                 }
             }
         }
