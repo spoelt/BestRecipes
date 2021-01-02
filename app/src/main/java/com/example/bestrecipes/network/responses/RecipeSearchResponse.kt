67eadce2aff111f6dff76fe5cc3c020d089c2804
spoelt <s.poeltinger@gmail.com>
2021-01-02 15:09:04 +0100

    setup network layer (Retrofit service + response class)

---
+++ app/src/main/java/com/example/bestrecipes/network/responses/RecipeSearchResponse.kt
@@ -0,0 +1,15 @@
+package com.example.bestrecipes.network.responses
+
+import com.example.bestrecipes.network.model.RecipeNetworkEntity
+import com.google.gson.annotations.SerializedName
+
+data class RecipeSearchResponse(
+
+    @SerializedName("count")
+    var count: Int,
+
+    // you could also add "next" and "previous" if you want to use pagination
+
+    @SerializedName("results")
+    var recipes: List<RecipeNetworkEntity>,
+)
