7376b2e9df158b682ee4c83f82f1388b0344f939
spoelt <s.poeltinger@gmail.com>
2021-01-06 19:07:22 +0100

    add asynchronous loading of recipe images

--- app/src/main/java/com/example/bestrecipes/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/example/bestrecipes/presentation/components/RecipeCard.kt
@@ -9,11 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.example.bestrecipes.R
 import com.example.bestrecipes.domain.model.Recipe
+import com.example.bestrecipes.util.DEFAULT_RECIPE_IMAGE
+import com.example.bestrecipes.util.loadPicture
 
 @Composable
 fun RecipeCard(
@@ -33,13 +36,16 @@ fun RecipeCard(
     ) {
         Column {
             recipe.featuredImage?.let { url ->
-                Image(
-                    bitmap = imageResource(id = R.drawable.empty_plate),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(225.dp),
-                    contentScale = ContentScale.Crop
-                )
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let {
+                    Image(
+                        bitmap = image.asImageBitmap(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(225.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
             recipe.title?.let { title ->
                 Row(
