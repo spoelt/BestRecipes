3094b252a4d9b3f665af43886eeed563c4341dc7
spoelt <s.poeltinger@gmail.com>
2021-01-07 23:09:07 +0100

    add search bar to RecipeListFragment.kt

--- app/src/main/java/com/example/bestrecipes/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/bestrecipes/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,20 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.example.bestrecipes.R
 import com.example.bestrecipes.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -42,12 +44,60 @@ class RecipeListFragment : Fragment() {
             setContent {
                 // "observe" recipes in Composable:
                 val recipes = viewModel.recipes.value
+                // keep track of search query
+                val query = viewModel.query.value
 
-                LazyColumn {
-                    itemsIndexed(
-                        items = recipes
-                    ) { index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = { })
+                Column {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colors.primary,
+                        elevation = 8.dp
+                    ) {
+                        Column {
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                TextField(
+                                    modifier = Modifier
+                                        .fillMaxWidth(0.85f)
+                                        .padding(8.dp),
+                                    value = query,
+                                    onValueChange = { newValue ->
+                                        viewModel.onQueryChanged(newValue)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                        // allowed input
+                                        keyboardType = KeyboardType.Text,
+                                        // icon at bottom of keyboard
+                                        imeAction = ImeAction.Search,
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search)
+                                    },
+                                    onImeActionPerformed = { action, softKeyboardController ->
+                                        if (action == ImeAction.Search) {
+                                            viewModel.newSearch(query)
+                                            softKeyboardController?.hideSoftwareKeyboard()
+                                        }
+                                    },
+                                    textStyle = TextStyle(
+                                        color = MaterialTheme.colors.onSurface
+                                    ),
+                                    backgroundColor = MaterialTheme.colors.surface
+                                )
+                            }
+                        }
+                    }
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = { })
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/bestrecipes/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/bestrecipes/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -25,18 +25,24 @@ class RecipeListViewModel @ViewModelInject constructor(
     // optimized MutableState for Compose:
     val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
 
+    val query = mutableStateOf("Chicken")
+
     init {
-        newSearch()
+        newSearch("Chicken")
     }
 
-    private fun newSearch() {
+    fun newSearch(query: String) {
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = "chicken"
+                query = query
             )
             recipes.value = result
         }
+    }
+
+    fun onQueryChanged(query: String) {
+        this.query.value = query
     }
 }
